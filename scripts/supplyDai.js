f28efc5505f8859f0b7be26089f45a6274dfa5f8
David <rajaroy674@gmail.com>
2021-05-04 14:04:20 +0530

    Added FOR USDT

--- scripts/supplyDai.js
+++ scripts/supplyDai.js
@@ -3,24 +3,24 @@ const CodingTest = artifacts.require("CodingTest");
 
 module.exports = async (done) => {
   const [recipient, _] = await web3.eth.getAccounts();
-  const dai = await IERC20.at("0xff795577d9ac8bd7d90ee22b6c1703490b6512fd");
-  const aDai = await IERC20.at("0xdcf0af9e59c002fa3aa091a46196b37530fd48a8");
-  console.log("aDai balance before depositing dai", (await aDai.balanceOf(recipient)).toString());
+  const usdt = await IERC20.at("0x13512979ade267ab5100878e2e0f485b568328a4");
+  const aUSDT = await IERC20.at("0xa01ba9fb493b851f4ac5093a324cb081a909c34b");
+  console.log("aUSDT balance before depositing usdt", (await aUSDT.balanceOf(recipient2)).toString());
   const codingTest = await CodingTest.deployed();
   const _calledContract = "0xe0fba4fc209b4948668006b2be61711b7f465bae";
-  const erc20Address = dai.address;
-  const onBehalfOf = recipient;
-  const amount = web3.utils.toWei("10", "ether"); //10 DAI tokens
-  const userAllowance = parseInt((await dai.allowance(recipient, codingTest.address)).toString());
-  console.log(userAllowance);
+  const erc20Address = usdt.address;
+  const onBehalfOf = recipient2;
+  const amount = web3.utils.toWei("10", "ether"); //10 usdt tokens
+  const userAllowance = parseInt((await usdt.allowance(recipient2, codingTest.address)).toString());
+  console.log("userAlloance for usdt", userAllowance);
   if (userAllowance < amount) {
     //Do unlimited approval here
-    console.log("Approving unlimited dai");
-    const totalSupply = await dai.totalSupply();
-    await dai.approve(codingTest.address, totalSupply);
+    console.log("Approving unlimited usdt");
+    const totalSupply = await usdt.totalSupply();
+    await usdt.approve(codingTest.address, totalSupply);
   }
-  console.log("Depositing 10 DAI to aave lending pool");
+  console.log("Depositing 10 usdt to aave lending pool");
   await codingTest.deposit(_calledContract, erc20Address, onBehalfOf, amount);
-  console.log("aDai balance After depositing dai", (await aDai.balanceOf(recipient)).toString());
+  console.log("aUSDT balance After depositing usdt", (await aUSDT.balanceOf(recipient2)).toString());
   done();
 };
--- scripts/withDrawDai.js
+++ scripts/withDrawDai.js
@@ -3,24 +3,25 @@ const CodingTest = artifacts.require("CodingTest");
 
 module.exports = async (done) => {
   const [recipient, _] = await web3.eth.getAccounts();
-  const dai = await IERC20.at("0xff795577d9ac8bd7d90ee22b6c1703490b6512fd");
-  const aDai = await IERC20.at("0xdcf0af9e59c002fa3aa091a46196b37530fd48a8");
-  console.log("Dai balance before withdrawing aDAI", (await dai.balanceOf(recipient)).toString());
+  const usdt = await IERC20.at("0x13512979ade267ab5100878e2e0f485b568328a4");
+  const aUSDT = await IERC20.at("0xa01ba9fb493b851f4ac5093a324cb081a909c34b");
+  console.log("usdt balance before withdrawing aUSDT", (await usdt.balanceOf(recipient)).toString());
   const codingTest = await CodingTest.deployed();
   const _calledContract = "0xe0fba4fc209b4948668006b2be61711b7f465bae";
-  const erc20Address = dai.address;
+  const erc20Address = usdt.address;
   const to = recipient;
   const amount = web3.utils.toWei("1", "ether");
-  const userAllowance = parseInt((await aDai.allowance(recipient, codingTest.address)).toString());
+  const userAllowance = parseInt((await aUSDT.allowance(recipient, codingTest.address)).toString());
   if (userAllowance < amount) {
+    aUSDT;
     //Do unlimited approval here
-    console.log("Approving unlimited adai");
-    const totalSupply = await aDai.totalSupply();
-    await adai.approve(codingTest.address, totalSupply);
-    console.log(parseInt((await aDai.allowance(recipient, codingTest.address)).toString()));
+    console.log("Approving unlimited aUSDT");
+    const totalSupply = await aUSDT.totalSupply();
+    await aUSDT.approve(codingTest.address, totalSupply);
+    console.log(parseInt((await aUSDT.allowance(recipient, codingTest.address)).toString()));
   }
-  console.log("Withdrawing 1Dai from lending pool");
+  console.log("Withdrawing usdt from lending pool");
   await codingTest.withdraw(_calledContract, erc20Address, amount, to);
-  console.log("Dai balance After Withdrawing aDAI", (await dai.balanceOf(recipient)).toString());
+  console.log("usdt balance After Withdrawing aUSDT", (await usdt.balanceOf(recipient)).toString());
   done();
 };
