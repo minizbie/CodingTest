b86696931cb7541cc05e6eead0455034b80d9245
David <rajaroy674@gmail.com>
2021-05-05 02:23:50 +0530

    adding lending pool address initial migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,7 @@
 const CodingTest = artifacts.require("CodingTest");
 const { deployProxy, upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 module.exports = async function (deployer, network, accounts) {
-  const instance = await deployProxy(CodingTest, { deployer });
+  const lendingPoolAddress = "0xe0fba4fc209b4948668006b2be61711b7f465bae";
+  const instance = await deployProxy(CodingTest, [lendingPoolAddress], { deployer });
   console.log("Deployed", instance.address);
 };
